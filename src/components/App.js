248983e66456ac8ea14dd5035364cf4f1ab4cf51
Oleg Dramaretskyi <dramarec@gmail.com>
2021-02-27 11:34:45 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Home Work #2 Feedback</h1>
+            <header className="App-header"></header>
+        </div>
+    );
+}
+
+export default App;
